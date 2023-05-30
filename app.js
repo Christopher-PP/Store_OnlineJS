f1ddf3fe78c6ea44423d9ca9104212c39b67ad8c
Christopher <alucart9@gmail.com>
2023-05-29 19:52:32 -0700

    Nav done

--- app.js
+++ app.js
@@ -23,12 +23,16 @@ mongoose
   .catch((error) => console.log(error));
 
 //Templates engine
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 //Archivos estaticos
 app.use(express.static(__dirname + "/public"));
 
+//rutas
+app.use("/client", require("./router/client"));
+app.use("/admin", require("./router/admin"));
+app.use("/", require("./router/webRutas"));
 //MIDDLEWARE
 app.use((req, res, next) => {
   res.status(404).render("404", {
@@ -37,9 +41,6 @@ app.use((req, res, next) => {
   });
 });
 
-//rutas
-app.use("/", require("./router/client"));
-app.use("/admin", require("./router/admin"));
 //Levantamiento de servidor express
 app.listen(process.env.PORT, () => {
   console.log("Server Started on port".blue, process.env.PORT);
